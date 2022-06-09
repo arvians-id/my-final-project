e69dbe0ce9e19b8354c6104efdcc6e9c7c40ad67
abid313 <abidfajar50@gmail.com>
2022-06-09 07:00:03 +0700

    refactor(user): modify user struct

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -3,14 +3,13 @@ package entity
 import "time"
 
 type User struct {
-	ID            int
-	Name          string
-	Unique_number string
-	Phone         string
-	Email         string
-	Password      string
-	Role          int
-	Image         string
-	Created_at    time.Time
-	Updated_at    time.Time
+	ID                int
+	Name              string
+	UserName          string
+	Email             string
+	Password          string
+	Role              string
+	EmailVerification time.Time
+	Created_at        time.Time
+	Updated_at        time.Time
 }
