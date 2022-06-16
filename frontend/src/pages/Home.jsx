c18646b7bd501c64b4dbf3e03e83b08cfb843300
irfan858 <irfankurniawan858@gmail.com>
2022-06-16 20:06:22 +0700

    fix: Fix Conflict Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Box, Flex } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
@@ -23,53 +22,4 @@ export default function Home() {
       </Flex>
     </div>
   )
-=======
-import {
-    Drawer,
-    DrawerBody,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    Button,
-    useDisclosure,
-    Link,
-    Heading,
-} from '@chakra-ui/react';
-import { HamburgerIcon } from '@chakra-ui/icons';
-import React from 'react';
-import useStore from '../provider/zustand/store';
-
-export default function Home() {
-    const user = useStore((state) => state.user);
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [placement, setPlacement] = React.useState('left');
-
-    return (
-        <>
-            <Button colorScheme="blue" onClick={onOpen}>
-                <HamburgerIcon />
-            </Button>
-            <Heading>Hy {user.username}, Selamat Datang</Heading>
-            <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerHeader borderBottomWidth="1px">
-                        Dashboard
-                    </DrawerHeader>
-                    <DrawerBody>
-                        <p>
-                            <Link>Series</Link>
-                        </p>
-                        <p>
-                            <Link>Courses</Link>
-                        </p>
-                        <p>
-                            <Link>Account</Link>
-                        </p>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
-        </>
-    );
->>>>>>> 6a615741ffc3f58b296426e7e401eff7f49bbfa4
 }
