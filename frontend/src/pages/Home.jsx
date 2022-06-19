3740c55960fff67f71cdbea44c01f7edf173748c
Mohd Ryan Obillah <ryanobillah@gmail.com>
2022-06-19 19:40:49 +0700

    feat: Add Course Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Box, Flex } from '@chakra-ui/react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
+    return (
+        <div>
+            <Navbar />
 
-      <Flex direction="row" justifyContent="center" alignItems="center">
-        <Flex width="20%" minHeight="90vh" bgColor="grey.100" boxShadow='dark-lg'>
-          <Sidebar />
-        </Flex>
+            <Flex direction="row" justifyContent="center" alignItems="center">
+                <Flex
+                    width="20%"
+                    minHeight="90vh"
+                    bgColor="grey.100"
+                    boxShadow="dark-lg"
+                >
+                    <Sidebar />
+                </Flex>
 
-        <Flex width="80%" minHeight="90vh" bg="white">
-          <Box m={5}>
-            <Box as="h1" fontSize="xl" fontWeight="semibold" mb={2}>
-              Home
-            </Box>
-          </Box>
-        </Flex>
-      </Flex>
-    </div>
-  )
+                <Flex width="80%" minHeight="90vh" bg="white">
+                    <Box m={5}>
+                        <Box as="h1" fontSize="xl" fontWeight="semibold" mb={2}>
+                            Home
+                        </Box>
+                    </Box>
+                </Flex>
+            </Flex>
+        </div>
+    );
 }
