c91765623304b80732506a72ce3a9793a84c5b86
irfan858 <irfankurniawan858@gmail.com>
2022-06-20 13:33:23 +0700

    fix : Fixing Blank Page

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Course from './pages/Course'
 
 export default function Main() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
+            {/* <Route path="/course" element={<Course/>} /> */}
         </Routes>
     )
 }
