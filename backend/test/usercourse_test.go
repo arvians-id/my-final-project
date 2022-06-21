65dd2e2309965b9c646e419243de121699ee3395
abid313 <abidfajar50@gmail.com>
2022-06-21 14:40:42 +0700

    chore: add some struct

--- backend/test/usercourse_test.go
+++ backend/test/usercourse_test.go
@@ -1,13 +1,35 @@
 package test_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
+	"github.com/rg-km/final-project-engineering-12/backend/entity"
+	"github.com/rg-km/final-project-engineering-12/backend/model"
 	// "github.com/rg-km/final-project-engineering-12/backend/test"
 )
 
 var _ = Describe("Test", func() {
+	var dummy entity.UserCourse
+	var uscourse model.CreateUserCourseRequest
+
+	dummy = entity.UserCourse{
+		UserId:   5,
+		CourseId: 3,
+	}
+
+	
+
+
+	fmt.Println(dummy)
+	fmt.Println(uscourse)
+
+
+
+
+
+
 	Describe("Add", func() {
 
 		It("adds two numbers", func() {
@@ -15,4 +37,5 @@ var _ = Describe("Test", func() {
 			Expect(sum).To(Equal(5))
 		})
 	})
+
 })
