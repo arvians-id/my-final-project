8b8b27dbc74713a5b083be29df41b2f4ef251523
abid313 <abidfajar50@gmail.com>
2022-06-23 21:40:10 +0700

    chore: modified testing without findAll

--- backend/test/unit/usercourse_test.go
+++ backend/test/unit/usercourse_test.go
@@ -99,10 +99,10 @@ var _ = Describe("User Course API", func() {
 		}
 	})
 
-	Describe("Create User Course and List User Course", func() {
+	Describe("Create User Course", func() {
 		When("Data is empty", func() {
-			It("should return All User Course", func() {
-				// Create User Course 1
+			It("should return Data User Course", func() {
+				// Create User Course
 				requestBody := strings.NewReader(`{"user_id": 10,"course_id": 1}`)
 				request := httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
 				request.Header.Add("Content-Type", "application/json")
@@ -111,54 +111,16 @@ var _ = Describe("User Course API", func() {
 				writer := httptest.NewRecorder()
 				server.ServeHTTP(writer, request)
 
-				// Create User Course 2
-				requestBody = strings.NewReader(`{"user_id": 10,"course_id": 2}`)
-				request = httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
-				request.Header.Add("Content-Type", "application/json")
-				request.Header.Set("Authorization", token)
-
-				writer = httptest.NewRecorder()
-				server.ServeHTTP(writer, request)
-
-				// Create User Course 3
-				requestBody = strings.NewReader(`{"user_id": 11,"course_id": 1}`)
-				request = httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
-				request.Header.Add("Content-Type", "application/json")
-				request.Header.Set("Authorization", token)
-
-				writer = httptest.NewRecorder()
-				server.ServeHTTP(writer, request)
-
-				// find all user courses
-				request = httptest.NewRequest(http.MethodGet, "/api/usercourse", nil)
-				request.Header.Add("Content-Type", "application/json")
-				request.Header.Set("Authorization", token)
-
-				writer = httptest.NewRecorder()
-				server.ServeHTTP(writer, request)
-
 				response := writer.Result()
 
 				Body, _ := io.ReadAll(response.Body)
 				var responseBody map[string]interface{}
 				_ = json.Unmarshal(Body, &responseBody)
 
-				usercourse := responseBody["data"].([]interface{})
-				usercourse1 := usercourse[0].(map[string]interface{})
-				usercourse2 := usercourse[1].(map[string]interface{})
-				usercourse3 := usercourse[2].(map[string]interface{})
-
 				Expect(int(responseBody["code"].(float64))).To(Equal(http.StatusOK))
-				Expect(responseBody["status"]).To(Equal("OK"))
-
-				Expect(usercourse1["user_id"]).To(Equal(10))
-				Expect(usercourse1["course_id"]).To(Equal(1))
-
-				Expect(usercourse2["user_id"]).To(Equal(10))
-				Expect(usercourse2["course_id"]).To(Equal(2))
-
-				Expect(usercourse3["user_id"]).To(Equal(11))
-				Expect(usercourse3["course_id"]).To(Equal(1))
+				Expect(responseBody["status"]).To(Equal("course successfully created"))
+				Expect(responseBody["data"].(map[string]interface{})["user_id"]).To(Equal(10))
+				Expect(responseBody["data"].(map[string]interface{})["course_id"]).To(Equal(1))
 			})
 		})
 	})
