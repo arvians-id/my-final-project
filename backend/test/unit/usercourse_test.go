ad12bf6880be05df4afaaf167e3883d49a40ac39
abid313 <abidfajar50@gmail.com>
2022-06-23 08:56:01 +0700

    chore: modified usercourse_test

--- backend/test/unit/usercourse_test.go
+++ backend/test/unit/usercourse_test.go
@@ -1,17 +1,106 @@
 package unit_test
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	// "github.com/rg-km/final-project-engineering-12/backend/test"
+	"github.com/rg-km/final-project-engineering-12/backend/config"
+	"github.com/rg-km/final-project-engineering-12/backend/test/setup"
 )
 
 var _ = Describe("Test", func() {
-	Describe("Add", func() {
+	var server *gin.Engine
 
-		It("adds two numbers", func() {
-			sum := 5
-			Expect(sum).To(Equal(5))
+	BeforeEach(func() {
+		configuration := config.New("../../.env.test")
+
+		_, err := setup.SuiteSetup(configuration)
+		if err != nil {
+			panic(err)
+		}
+
+		router := setup.ModuleSetup(configuration)
+		server = router
+	})
+
+	AfterEach(func() {
+		configuration := config.New("../../.env.test")
+		db, err := setup.SuiteSetup(configuration)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		err = setup.TearDownTest(db)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	Describe("Create User Course", func() {
+		When("Data is empty", func() {
+			It("should return All User Course", func() {
+				// Create User Course 1
+				requestBody := strings.NewReader(`{"user_id": 1,"course_id": 1}`)
+				request := httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				request.Header.Add("Content-Type", "application/json")
+
+				writer := httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				// Create User Course 2
+				requestBody = strings.NewReader(`{"user_id": 1,"course_id": 2}`)
+				request = httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				request.Header.Add("Content-Type", "application/json")
+
+				writer = httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				// Create User Course 3
+				requestBody = strings.NewReader(`{"user_id": 2,"course_id": 1}`)
+				request = httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				request.Header.Add("Content-Type", "application/json")
+
+				writer = httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				// find all user courses
+				request = httptest.NewRequest(http.MethodGet, "/api/usercourses", nil)
+				request.Header.Add("Content-Type", "application/json")
+
+				writer = httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				Body, err := ioutil.ReadAll(writer.Body)
+				if err != nil {
+					panic(err)
+				}
+				var responseBody map[string]interface{}
+				_ = json.Unmarshal(Body, &responseBody)
+
+				usercourse := responseBody["data"].([]interface{})
+				usercourse1 := usercourse[0].(map[string]interface{})
+				usercourse2 := usercourse[1].(map[string]interface{})
+				usercourse3 := usercourse[2].(map[string]interface{})
+
+				Expect(int(responseBody["code"].(float64))).To(Equal(http.StatusOK))
+				Expect(responseBody["status"]).To(Equal("OK"))
+
+				Expect(usercourse1["user_id"]).To(Equal(1))
+				Expect(usercourse1["course_id"]).To(Equal(1))
+
+				Expect(usercourse2["user_id"]).To(Equal(1))
+				Expect(usercourse2["course_id"]).To(Equal(2))
+
+				Expect(usercourse3["user_id"]).To(Equal(2))
+				Expect(usercourse3["course_id"]).To(Equal(1))
+			})
 		})
 	})
 })
