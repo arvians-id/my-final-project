076eaa93e4518a70112da68f3dee73382fe0b15f
abid313 <abidfajar50@gmail.com>
2022-06-23 09:17:04 +0700

    chore: modified usercourse_test.go

--- backend/test/unit/usercourse_test.go
+++ backend/test/unit/usercourse_test.go
@@ -47,31 +47,31 @@ var _ = Describe("User Course API", func() {
 		When("Data is empty", func() {
 			It("should return All User Course", func() {
 				// Create User Course 1
-				requestBody := strings.NewReader(`{"user_id": 1,"course_id": 1}`)
-				request := httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				requestBody := strings.NewReader(`{"user_id": 10,"course_id": 1}`)
+				request := httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
 				request.Header.Add("Content-Type", "application/json")
 
 				writer := httptest.NewRecorder()
 				server.ServeHTTP(writer, request)
 
 				// Create User Course 2
-				requestBody = strings.NewReader(`{"user_id": 1,"course_id": 2}`)
-				request = httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				requestBody = strings.NewReader(`{"user_id": 10,"course_id": 2}`)
+				request = httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
 				request.Header.Add("Content-Type", "application/json")
 
 				writer = httptest.NewRecorder()
 				server.ServeHTTP(writer, request)
 
 				// Create User Course 3
-				requestBody = strings.NewReader(`{"user_id": 2,"course_id": 1}`)
-				request = httptest.NewRequest(http.MethodPost, "/api/usercourses", requestBody)
+				requestBody = strings.NewReader(`{"user_id": 11,"course_id": 1}`)
+				request = httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
 				request.Header.Add("Content-Type", "application/json")
 
 				writer = httptest.NewRecorder()
 				server.ServeHTTP(writer, request)
 
 				// find all user courses
-				request = httptest.NewRequest(http.MethodGet, "/api/usercourses", nil)
+				request = httptest.NewRequest(http.MethodGet, "/api/usercourse", nil)
 				request.Header.Add("Content-Type", "application/json")
 
 				writer = httptest.NewRecorder()
@@ -92,13 +92,13 @@ var _ = Describe("User Course API", func() {
 				Expect(int(responseBody["code"].(float64))).To(Equal(http.StatusOK))
 				Expect(responseBody["status"]).To(Equal("OK"))
 
-				Expect(usercourse1["user_id"]).To(Equal(1))
+				Expect(usercourse1["user_id"]).To(Equal(10))
 				Expect(usercourse1["course_id"]).To(Equal(1))
 
-				Expect(usercourse2["user_id"]).To(Equal(1))
+				Expect(usercourse2["user_id"]).To(Equal(10))
 				Expect(usercourse2["course_id"]).To(Equal(2))
 
-				Expect(usercourse3["user_id"]).To(Equal(2))
+				Expect(usercourse3["user_id"]).To(Equal(11))
 				Expect(usercourse3["course_id"]).To(Equal(1))
 			})
 		})
