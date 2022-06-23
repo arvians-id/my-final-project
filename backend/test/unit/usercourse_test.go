402f54f2b9e9abce5c4addaca30de00898f82507
abid313 <abidfajar50@gmail.com>
2022-06-23 18:02:12 +0700

    chore: usercourse test done but login still error

--- backend/test/unit/usercourse_test.go
+++ backend/test/unit/usercourse_test.go
@@ -115,7 +115,7 @@ var _ = Describe("User Course API", func() {
 		}
 	})
 
-	Describe("Create User Course", func() {
+	Describe("Create User Course and List User Course", func() {
 		When("Data is empty", func() {
 			It("should return All User Course", func() {
 				// Create User Course 1
@@ -175,6 +175,68 @@ var _ = Describe("User Course API", func() {
 
 				Expect(usercourse3["user_id"]).To(Equal(11))
 				Expect(usercourse3["course_id"]).To(Equal(1))
+			})
+		})
+	})
+	Describe("Get User Course By UserId and CourseId", func() {
+		When("Data is empty", func() {
+			It("should return User Course", func() {
+				// Create User Course 1
+				requestBody := strings.NewReader(`{"user_id": 10,"course_id": 1}`)
+				request := httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
+				request.Header.Add("Content-Type", "application/json")
+				request.Header.Set("Authorization", token)
+
+				writer := httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+				// Get User Course By UserId and CourseId
+				request = httptest.NewRequest(http.MethodGet, "/api/usercourse/10/1", nil)
+				request.Header.Add("Content-Type", "application/json")
+				request.Header.Set("Authorization", token)
+
+				writer = httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				response := writer.Result()
+
+				Body, _ := io.ReadAll(response.Body)
+				var responseBody map[string]interface{}
+				_ = json.Unmarshal(Body, &responseBody)
+
+				usercourse := responseBody["data"].(map[string]interface{})
+
+				Expect(usercourse["user_id"]).To(Equal(10))
+				Expect(usercourse["course_id"]).To(Equal(1))
+			})
+		})
+	})
+	Describe("Delete User Course By UserId and CourseId", func() {
+		When("Data is empty", func() {
+			It("should return User Course", func() {
+				// Create User Course 1
+				requestBody := strings.NewReader(`{"user_id": 10,"course_id": 1}`)
+				request := httptest.NewRequest(http.MethodPost, "/api/usercourse", requestBody)
+				request.Header.Add("Content-Type", "application/json")
+				request.Header.Set("Authorization", token)
+
+				writer := httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+				// Delete User Course By UserId and CourseId
+				request = httptest.NewRequest(http.MethodDelete, "/api/usercourse/10/1", nil)
+				request.Header.Add("Content-Type", "application/json")
+				request.Header.Set("Authorization", token)
+
+				writer = httptest.NewRecorder()
+				server.ServeHTTP(writer, request)
+
+				response := writer.Result()
+
+				Body, _ := io.ReadAll(response.Body)
+				var responseBody map[string]interface{}
+				_ = json.Unmarshal(Body, &responseBody)
+
+				Expect(int(responseBody["code"].(float64))).To(Equal(http.StatusOK))
+				Expect(responseBody["status"]).To(Equal("OK"))
 			})
 		})
 	})
