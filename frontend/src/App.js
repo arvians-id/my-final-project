f67e2c96726bd2c53aab7013c9a76cb1d620c1d5
Mohd Ryan Obillah <ryanobillah@gmail.com>
2022-06-25 17:45:13 +0700

    feat: Add CourseDetail Page, Add DetailCard and ListModule component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Main from './Main';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import useStore from './provider/zustand/store';
-import { adapterUserToFE } from './utils/adapterToFE'
+import { adapterUserToFE } from './utils/adapterToFE';
 import Submission from './pages/Submission';
 import Home from './pages/Home';
 import CoursePage from './pages/Course';
@@ -14,82 +14,77 @@ import Profile from './pages/Profile';
 import DashboardDataSiswa from './pages/DashboardDataSiswa';
 import HomeNonSiswa from './pages/HomeNonSiswa';
 import DashboardDataPengguna from './pages/DashboardDataPengguna';
+import CourseDetail from './pages/CourseDetail';
 
 function App() {
-    const user = useStore((state) => state.user);
-    const setUser = useStore((state) => state.setUser);
-    const [isReady, setIsReady] = useState(false);
+  const user = useStore((state) => state.user);
+  const setUser = useStore((state) => state.setUser);
+  const [isReady, setIsReady] = useState(false);
 
-    const checkLogin = async () => {
-        const res = await API_CHECK_STATUS();
-        // kalo oke, berarti set user di zustand
-        if (res.status === 200) {
-            setUser(adapterUserToFE(res.data.data));
-        }
-        console.log('user', user)
-        setIsReady(true);
-    };
-    // API_CHECK_STATUS
-    useEffect(() => {
-        checkLogin();
-    }, []);
+  const checkLogin = async () => {
+    const res = await API_CHECK_STATUS();
+    // kalo oke, berarti set user di zustand
+    if (res.status === 200) {
+      setUser(adapterUserToFE(res.data.data));
+    }
+    console.log('user', user);
+    setIsReady(true);
+  };
+  // API_CHECK_STATUS
+  useEffect(() => {
+    checkLogin();
+  }, []);
 
-    if (!isReady) return null;
+  if (!isReady) return null;
 
-    return (
-        <>
-            {user !== undefined ? (
-                user.role === "Siswa" ?
-                    <Routes>
-                        <Route
-                            path="/login"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route
-                            path="/register"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route path="home" element={<Navigate to="/" replace />} />
-                        <Route path="course" element={<CoursePage replace />} />
-                        <Route path="submission" element={<Submission replace />} />
-                        <Route path="discussion" element={<Discussion replace />} />
-                        <Route path="profile" element={<Profile replace />} />
-                        <Route path="/" element={<Home replace />} />
-                    </Routes> : user.role === "Guru" ? <Routes>
-                        <Route
-                            path="/login"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route
-                            path="/register"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route path="/" element={<HomeNonSiswa replace />} />
-                        <Route path="dashboard-siswa" element={<DashboardDataSiswa replace />} />
-                        <Route path="profile" element={<Profile replace />} />
-                    </Routes> : <Routes>
-                        <Route
-                            path="/login"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route
-                            path="/register"
-                            element={<Navigate to="/" replace />}
-                        />
-                        <Route path="/" element={<HomeNonSiswa replace />} />
-                        <Route path="dashboard-pengguna" element={<DashboardDataPengguna replace />} />
-                        <Route path="profile" element={<Profile replace />} />
-                    </Routes>
-            ) : (
-                <Routes>
-                    <Route path="*" element={<Login />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/" element={<Login />} />
-                </Routes>
-            )}
-        </>
-    );
+  return (
+    <>
+      {user !== undefined ? (
+        user.role === 'Siswa' ? (
+          <Routes>
+            <Route path="/login" element={<Navigate to="/" replace />} />
+            <Route path="/register" element={<Navigate to="/" replace />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="course" element={<CoursePage replace />} />
+            <Route path="coursedetail" element={<CourseDetail />} />
+            <Route path="submission" element={<Submission replace />} />
+            <Route path="discussion" element={<Discussion replace />} />
+            <Route path="profile" element={<Profile replace />} />
+            <Route path="/" element={<Home replace />} />
+          </Routes>
+        ) : user.role === 'Guru' ? (
+          <Routes>
+            <Route path="/login" element={<Navigate to="/" replace />} />
+            <Route path="/register" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<HomeNonSiswa replace />} />
+            <Route
+              path="dashboard-siswa"
+              element={<DashboardDataSiswa replace />}
+            />
+            <Route path="profile" element={<Profile replace />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Navigate to="/" replace />} />
+            <Route path="/register" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<HomeNonSiswa replace />} />
+            <Route
+              path="dashboard-pengguna"
+              element={<DashboardDataPengguna replace />}
+            />
+            <Route path="profile" element={<Profile replace />} />
+          </Routes>
+        )
+      ) : (
+        <Routes>
+          <Route path="*" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      )}
+    </>
+  );
 }
 
 export default App;
